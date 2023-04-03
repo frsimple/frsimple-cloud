f2724b253ffe16b8884a37d171448a62acabcd50
zhoupengyu <18721015672@163.com>
2023-04-03 17:03:57 +0800

    代码合并问题修复

--- service/simple-plan/src/main/java/org/simple/plan/controller/InwardController.java
+++ service/simple-plan/src/main/java/org/simple/plan/controller/InwardController.java
@@ -1,24 +1,7 @@
 package org.simple.plan.controller;
 
-import cn.binarywang.wx.miniapp.api.WxMaService;
-import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
-import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
-import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
-import cn.hutool.json.JSONObject;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import me.chanjar.weixin.common.error.WxErrorException;
-import org.simple.common.utils.CommonResult;
-import org.simple.common.utils.RedomUtil;
-import org.simple.plan.entity.DesignUser;
-import org.simple.plan.service.DesignUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 
 /***
